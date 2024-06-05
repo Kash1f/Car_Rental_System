5990a8f319f754dd3f0e70a4699f70da96db06a6
Kashif <ahmed.kashif1001@gmail.com>
2024-06-05 23:47:45 +0500

    abstract employee class

--- src/CarRentalSystem/Main.java
+++ src/CarRentalSystem/Main.java
@@ -1,5 +1,12 @@
 package CarRentalSystem;
 
+abstract class Employee{
+
+    private String name;
+    private int id;
+
+}
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("Car Rental System");
